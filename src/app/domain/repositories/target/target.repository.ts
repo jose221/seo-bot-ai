e9357398cc5aed6f889127d211fa91f1473edf03
jose221 <ext.angel.alvarado@pricetravel.com>
2025-12-17 10:58:21 -0500

    feat: introduce initial mockup for SEO audit form
    - Added `audit_form.html` as the UI for web audit feature.
    - Created form for selecting websites and enabling AI-based analysis.
    - Incorporated a modal for audit status and task details.
    - Integrated essential assets including Bootstrap.

---
+++ src/app/domain/repositories/target/target.repository.ts
@@ -0,0 +1,9 @@
+import {CreateTargetRequestModel, FilterTargetRequestModel, UpdateTargetRequestModel} from '@/app/domain/models/target/request/target-request.model';
+import {TargetResponseModel} from '@/app/domain/models/target/response/target-response.model';
+
+export abstract class TargetRepository {
+  abstract create(params: CreateTargetRequestModel): Promise<any>;
+  abstract delete(id: number): Promise<any>;
+  abstract get(params?: FilterTargetRequestModel): Promise<TargetResponseModel[]>;
+  abstract find(id: number): Promise<TargetResponseModel>;
+}
---
+++ src/app/infrastructure/repositories/target/target.implementation.repository.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {TargetRepository} from '@/app/domain/repositories/target/target.repository';
+import {CreateTargetRequestModel, FilterTargetRequestModel, UpdateTargetRequestModel} from '@/app/domain/models/target/request/target-request.model';
+import {TargetResponseModel} from '@/app/domain/models/target/response/target-response.model';
+import {TargetService} from '@/app/infrastructure/services/target/target.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TargetImplementationRepository implements TargetRepository {
+  constructor(private primaryService: TargetService) {
+
+  }
+  async create(params: CreateTargetRequestModel): Promise<any> {
+    return await this.primaryService.create(params);
+  }
+  async delete(id: number): Promise<any> {
+    return await this.primaryService.delete(id);
+  }
+  async get(params?: FilterTargetRequestModel): Promise<TargetResponseModel[]> {
+    return await this.primaryService.get(params);
+  }
+  async find(id: number): Promise<TargetResponseModel> {
+    return await this.primaryService.find(id);
+  }
+}
