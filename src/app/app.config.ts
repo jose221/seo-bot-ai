63f1b747e48a0e5f1a956b4d60d6b71ba0fe4f4b
jose221 <ext.angel.alvarado@pricetravel.com>
2025-12-17 10:58:43 -0500

    feat: register TargetRepository in appConfig
    - Added `TargetRepository` and `TargetImplementationRepository` to the app's providers.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,12 +9,15 @@ import {
 import {TranslationRepository} from '@/app/domain/repositories/translation/translation.repository';
 import {AuthImplementationRepository} from '@/app/infrastructure/repositories/auth/auth.implementation.repository';
 import {AuthRepository} from '@/app/domain/repositories/auth/auth.repository';
+import {TargetRepository} from '@/app/domain/repositories/target/target.repository';
+import {TargetImplementationRepository} from '@/app/infrastructure/repositories/target/target.implementation.repository';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), provideClientHydration(withEventReplay()),
     { provide: TranslationRepository, useClass: TranslationImplementationRepository },
-    { provide: AuthRepository, useClass: AuthImplementationRepository }
+    { provide: AuthRepository, useClass: AuthImplementationRepository },
+    { provide: TargetRepository, useClass: TargetImplementationRepository }
   ]
 };
