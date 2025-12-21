cb16381ea76466b7bcd59796cc8134fd29b8ae0c
jose221 <ext.angel.alvarado@pricetravel.com>
2025-12-20 20:56:28 -0500

    feat: replace `Target` component with `TargetList` and add modularized `Target` features
    - Removed the `Target` component and introduced `TargetList`, `TargetForm`, and their related templates, styles, and unit tests.
    - Updated route configuration to use `TargetList` as the new entry point for `target`.
    - Enhanced `Admin` layout with navigation and content styling.
    - Added modularized components for easier management of `target` features (`target-list`, `target-form`).
    - Improved page structure and user experience with grid and table views for `TargetList`.

---
+++ src/app/presentation/pages/admin/target/target-list/target-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-target-list',
+  imports: [],
+  templateUrl: './target-list.html',
+  styleUrl: './target-list.scss',
+})
+export class TargetList {
+
+}
