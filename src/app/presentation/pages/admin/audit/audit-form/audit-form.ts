fefd5d128fda84edce38202ea72de9c3c8293803
jose221 <ext.angel.alvarado@pricetravel.com>
2025-12-22 01:50:35 -0500

    feat: add `AuditForm` and `AuditList` components with templates, styles, and tests
    - Introduced `AuditForm` and `AuditList` components for managing audits in the admin module.
    - Created associated HTML, SCSS, and unit test files for both components.
    - Implemented reusable table and filter list configurations in `AuditList`.
    - Enhanced `AuditList` with features like pagination, modal display, and dynamic data population.

---
+++ src/app/presentation/pages/admin/audit/audit-form/audit-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audit-form',
+  imports: [],
+  templateUrl: './audit-form.html',
+  styleUrl: './audit-form.scss',
+})
+export class AuditForm {
+
+}
