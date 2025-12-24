a0ae80640a3d740ed9b8c6b81a95dd7ccc51ca2e
jose221 <ext.angel.alvarado@pricetravel.com>
2025-12-24 15:05:53 -0500

    feat: add login guard, default admin redirection, and enhance admin navigation
    - Introduced `loginGuard` to secure routes with authentication validation.
    - Added fallback route to redirect unknown paths to `/admin`.
    - Enhanced admin navigation with dynamic translations and logout functionality.
    - Updated Spanish (`es.json`) and English (`en.json`) translations for admin navbar.
    - Improved UI for admin navigation with language support and cleaner structure.
    - Updated FastAPI dependencies in `requirements.txt` for compatibility.

--- src/app/presentation/pages/admin/admin.ts
+++ src/app/presentation/pages/admin/admin.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import {Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {TranslatePipe} from '@ngx-translate/core';
+import {AuthRepository} from '@/app/domain/repositories/auth/auth.repository';
 
 @Component({
   selector: 'app-admin',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, TranslatePipe],
   templateUrl: './admin.html',
   styleUrl: './admin.scss',
 })
 export class Admin {
+  router = inject(Router);
+  authRepository = inject(AuthRepository);
+  isActive(route: string): boolean {
+    return this.router.url.includes(route);
+  }
+  async logout(){
+    this.authRepository.logout();
+    await this.router.navigateByUrl('/');
+  }
 
 }
